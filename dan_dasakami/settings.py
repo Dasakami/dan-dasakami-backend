b95c86b7042e7c4ddc352a92c100cd757c512ab4
Dasakami <dendasakami@gmail.com>
2025-11-27 13:22:25 +0600

    db edit port

--- dan_dasakami/settings.py
+++ dan_dasakami/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'PORT': 5432,
     }
 }
 
